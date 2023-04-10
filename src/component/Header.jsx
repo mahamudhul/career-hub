ddba251cd7c6e3a7763088f20c05ab6cead50fbe
mahamudhul <111944934+mahamudhul@users.noreply.github.com>
2023-04-10 21:16:19 +0600

    banner details and image added

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,22 +1,101 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import {
+    BoltIcon,
+    Bars3BottomRightIcon,
+    XMarkIcon,
+} from '@heroicons/react/24/solid'
+
 
 const Header = () => {
+    const [menu, setMenu] = useState(false)
+
+
     return (
-        <nav className='flex justify-between'>
-            <div>
-                <h1 className='font-bold text-xl'>PREMIER BANK</h1>
+        <div >
+            <nav className='flex justify-between'>
+                <div>
+                    <h1 className='font-bold text-xl'>PREMIER BANK</h1>
+                </div>
+                <div className='lg:flex gap-3 hidden'>
+                    <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : 'default')} >Home</NavLink>
+                    <NavLink to='/statistics' className={({ isActive }) => (isActive ? 'active' : 'default')}>Statistics</NavLink>
+                    <NavLink to='/jobs' className={({ isActive }) => (isActive ? 'active' : 'default')}>Applied Jobs</NavLink>
+                    <NavLink to='/blog' className={({ isActive }) => (isActive ? 'active' : 'default')}>Blog</NavLink>
+                </div>
+                <div>
+                    <button className='btn'>Star Applying</button>
+
+                </div>
+            </nav>
+            <div className='lg:hidden'>
+                {/* Dropdown Open Button */}
+                <button
+                    aria-label='Open Menu'
+                    title='Open Menu'
+                    onClick={() => setMenu(true)}
+                >
+                    <Bars3BottomRightIcon className='w-5 text-gray-600' />
+                </button>
+                {menu && (
+                    <div className='absolute top-0 left-0 w-full z-10'>
+                        <div className='p-5 bg-white border rounded shadow-sm'>
+                            {/* Logo & Button section */}
+                            <div className='flex items-center justify-between mb-4'>
+                                <div>
+                                    <h1 className='font-bold text-xl'>PREMIER BANK</h1>
+                                </div>
+                                {/* Dropdown menu close button */}
+                                <div>
+                                    <button
+                                        aria-label='Close Menu'
+                                        title='Close Menu'
+                                        onClick={() => setMenu(false)}
+                                    >
+                                        <XMarkIcon className='w-5 text-gray-600' />
+                                    </button>
+                                </div>
+                            </div>
+                            {/* Mobile Nav Items Section */}
+                            <nav>
+                                <ul className='space-y-4'>
+                                    <li>
+                                        <Link to='/' className='default'>
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            to='/statistics'
+                                            className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                                        >
+                                            Statistics
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            to='/jobs'
+                                            className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                                        >
+                                            Applied Jobs
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            to='/blog'
+                                            className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                                        >
+                                            Blog
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                )}
             </div>
-            <div className='flex gap-3'>
-                <Link to='/'>Home</Link>
-                <Link to='/statistics'>Statistics</Link>
-                <Link to='/jobs'>Applied Jobs</Link>
-                <Link to='/blog'>Blog</Link>
-            </div>
-            <div>
-                <button className='bg-sky-500 px-3 py-3 rounded-lg hover:bg-sky-600 text-white'>Star Applying</button>
-            </div>
-        </nav>
+        </div>
+
     );
 };
 
